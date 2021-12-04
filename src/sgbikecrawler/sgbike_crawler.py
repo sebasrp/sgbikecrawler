8f2004f1c25b2724254554388a76ed2791d29426
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2021-12-04 22:49:12 +0800

    crawl first page of sgbikemart (no pagination)

--- src/sgbikecrawler/sgbike_crawler.py
+++ src/sgbikecrawler/sgbike_crawler.py
@@ -1,9 +1,14 @@
+import json
 import click
+from sgbikemart import SGBikeMart
 
 
 @click.command()
-def main():
-    print("hello world")
+@click.option("-model", "--model", help="The bike model you are interested in")
+def main(model):
+    crawler = SGBikeMart()
+    bikes_list = crawler.retrieve_all_listings(bike_model="Yamaha")
+    print(json.dumps(bikes_list, indent=4))
 
 
 if __name__ == "__main__":
---
+++ src/sgbikecrawler/sgbikemart.py
@@ -0,0 +1,86 @@
+import regex as re
+
+from bs4 import BeautifulSoup
+import requests
+
+
+class SGBikeMart:
+    BASE = "https://www.sgbikemart.com.sg/"
+    URL = "https://www.sgbikemart.com.sg/listing/usedbikes/listing/"
+
+    @staticmethod
+    def retrieve_page(url):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+            "Accept-Encoding": "none",
+            "Accept-Language": "en-US,en;q=0.8",
+            "Connection": "keep-alive",
+        }
+        req = requests.get(url, headers=headers)
+        soup = BeautifulSoup(req.text, "html.parser")
+        return soup
+
+    @staticmethod
+    def retrieve_body_section(soup, section_str):
+        section_value = None
+        string_div = soup.find(text=re.compile(section_str))
+        if string_div is not None:
+            section_value = string_div.find_next(
+                "div", {"class": ["text-start"]}
+            ).text.strip()
+        return section_value
+
+    @staticmethod
+    def retrieve_price(soup):
+        price_value = ""
+        price = soup.find("sup", text="SGD")
+        if price is not None:
+            parent = price.parent
+            price_value_group = re.search(r"SGD\$\K\S+", parent.text)
+            price_value = price_value_group.group(0).strip()
+            print(price_value)
+        return price_value
+
+    def retrieve_all_listings(self, bike_model):
+        results = []
+
+        page_url = f"{SGBikeMart.URL}?bike_model={bike_model}"
+        bike_page = SGBikeMart.retrieve_page(page_url)
+        all_bikes = bike_page.select("div.row > div.col-lg-9 > div.card")
+
+        for bike in all_bikes:
+            ad_url = ""
+            ad_title = ""
+            header = bike.find("div", {"class": ["card-header"]})
+
+            if header is None:
+                continue
+
+            link = header.find("a", href=True)
+            if link is not None:
+                ad_url = f"{SGBikeMart.BASE}{link['href']}"
+                ad_title = link.get_text().strip()
+            body = bike.find(name="div", class_="card-body")
+            if body is not None:
+                reg_date = SGBikeMart.retrieve_body_section(body, "Reg Date")
+                capacity = SGBikeMart.retrieve_body_section(body, "Capacity")
+                bike_type = SGBikeMart.retrieve_body_section(body, "Vehicle Type")
+                mileage = SGBikeMart.retrieve_body_section(body, "Mileage")
+                price = SGBikeMart.retrieve_price(body)
+
+                bike_ad = {
+                    "title": ad_title,
+                    "url": ad_url,
+                    "reg_date": reg_date,
+                    "capacity": capacity,
+                    "bike_type": bike_type,
+                    "mileage": mileage,
+                    "price": price,
+                }
+                results.append(bike_ad)
+
+        print(f"Number of items: {len(all_bikes)}")
+        print(f"URL: {page_url}")
+        return results
